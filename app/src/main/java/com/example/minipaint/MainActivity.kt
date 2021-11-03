bf0bac9a5664dc9668a29ec6d67c1316a490163f
JulioCesar415 <35044318+JulioCesar415@users.noreply.github.com>
2021-11-02 20:55:09 -0700

    systemUIVisibility unresolved

--- app/src/main/java/com/example/minipaint/MainActivity.kt
+++ app/src/main/java/com/example/minipaint/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.minipaint
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+//        create instance
+        val myCanvasView = MyCanvasView(this)
+
+//        request full screen for layout of myCanvasView
+        myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
+
+//        add content description
+        myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
+
+//        set content view to myCanvasView
+        setContentView(myCanvasView)
     }
 }
--- app/src/main/java/com/example/minipaint/MyCanvasView.kt
+++ app/src/main/java/com/example/minipaint/MyCanvasView.kt
@@ -1,6 +1,48 @@
 package com.example.minipaint
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.view.View
+import androidx.core.content.res.ResourcesCompat
 
-class MyCanvasView (context: Context) : View(context) {}
+class MyCanvasView (context: Context) : View(context) {
+
+//    define member variables for canvas
+    private lateinit var extraCanvas: Canvas
+
+//    define member variable for bitmap
+    private lateinit var extraBitmap: Bitmap
+
+//    define class level variable for background color of the canvas
+    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+
+    //    this callback method is called by Android system with the changed screen dimensions,
+//    with new width and height (to change to) and old width and height (to change from)
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+//        instance with new w and h
+        extraBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+
+//        create Canvas instance
+        extraCanvas = Canvas(extraBitmap)
+
+//        specify background color in which to fill extraCanvas
+        extraCanvas.drawColor(backgroundColor)
+
+//        new bitmap and canvas are created every time function executes. We need a
+//        new bitmap because size has changed. However, this is a memory leak because
+//        we leave old bitmaps. To fix this recycle extraBitmap before creating the new one
+        if (::extraBitmap.isInitialized) extraBitmap.recycle()
+
+//        draw contents of the cached extraBitmap on the canvas associated with the view.
+
+
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(extraBitmap, 0f,0f, null)
+    }
+}
